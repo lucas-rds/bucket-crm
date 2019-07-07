14bf34f687bb94af2be9645083a678ab940db538
lucas-rds <lucas.rodrigues6673@gmail.com>
2019-07-07 17:54:29 -0300

    -

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-git update-index --chmod=+x gradlew
-
 # Decrypt config & key files
-openssl aes-256-cbc -K $encrypted_1437fc144fa9_key -iv $encrypted_1437fc144fa9_iv -in client-secret.json.enc -out client-secret.json -d
+# openssl aes-256-cbc -K $encrypted_1437fc144fa9_key -iv $encrypted_1437fc144fa9_iv -in client-secret.json.enc -out client-secret.json -d
 
 # Authorise GCloud SDK
-gcloud auth activate-service-account travis-crm@k-social-crm.iam.gserviceaccount.com --key-file=client-secret.json --project=k-social-crm
-gcloud components install app-engine-java
+# gcloud auth activate-service-account travis-crm@k-social-crm.iam.gserviceaccount.com --key-file=client-secret.json --project=k-social-crm
 
 # Run Gradle deployment task
 ./gradlew appengineDeploy
